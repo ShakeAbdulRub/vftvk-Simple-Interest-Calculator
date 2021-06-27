c8ff8048607bee5eebff33357abff25149fbe43a
Shake Abdul Rub <abdulrubshake7@gmail.com>
2021-06-27 06:27:18 +0000

    made changes

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years =document.getElementById("years").value;
+    var intrest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText="If you deposit "+principal;
+    document.getElementById("hello1").innerText="at an interest rate of  "+rate;
+    document.getElementById("hello2").innerText="You will receive an amount of "+intrest;
+    document.getElementById("hello3").innerText="in the year "+year;
+
+
+}
+function updateRate()
+{
+    var rateval =document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
